788884e6b094166c5156c5b86855b6fe693234f2
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-11-06 14:26:12 +0200

    Do task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+let timeoutId = null;
+let isStarted = false;
+
+refs.btnStart.addEventListener('click', onClickBtnStart);
+refs.btnStop.addEventListener('click', onClickBtnStop);
+
+function onClickBtnStart() {
+  if (isStarted) {
+    return;
+  }
+
+  paintBodyRecursive();
+  isStarted = true;
+}
+
+function onClickBtnStop() {
+  clearTimeout(timeoutId);
+  isStarted = false;
+}
+
+function paintBodyRecursive() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  timeoutId = setTimeout(paintBodyRecursive, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
