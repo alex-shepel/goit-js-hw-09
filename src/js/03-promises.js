47881818e8d950cd66e7d6e260ce68647f89dc52
AlexandrShepel <iam.alex.shepel@gmail.com>
2021-11-07 14:09:22 +0200

    Do task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('form'),
+};
+
+const createPromise = (position, delay) =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay),
+  );
+
+const onSubmit = e => {
+  e.preventDefault();
+
+  const data = {};
+
+  [...e.target.elements]
+    .filter(elem => elem.tagName === 'INPUT')
+    .forEach(elem => (data[elem.name] = parseInt(elem.value)));
+
+  for (let i = 1; i <= data.amount; i += 1) {
+    createPromise(i, data.delay + i * data.step)
+      .then(({ position, delay }) => {
+        Notify.success(`Resolved promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+      });
   }
-}
+};
+
+refs.form.addEventListener('submit', onSubmit);
